7ee38d8d4fde1624bf86870853fc98b56f4abc91
joaquimg <joaquimdgarcia@gmail.com>
2018-04-07 22:50:09 -0300

    copy test passing

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,11 +7,15 @@ const MOIGLP = MathOptInterfaceGLPK
     @testset "Linear tests" begin
         linconfig_nocertificate = MOIT.TestConfig(infeas_certificates=false)
         solver = GLPKOptimizerLP()
-        MOIT.contlineartest(solver, linconfig_nocertificate)
+        MOIT.contlineartest(solver, linconfig_nocertificate, ["linear10"])
         
         linconfig_nocertificate_noduals = MOIT.TestConfig(duals=false,infeas_certificates=false)
         solver_mip = GLPKOptimizerMIP()
-        MOIT.contlineartest(solver_mip, linconfig_nocertificate_noduals, ["linear8b","linear8c"])
+        MOIT.contlineartest(solver_mip, linconfig_nocertificate_noduals, ["linear10","linear8b","linear8c"])
+
+        # Intervals
+        linconfig_noquery = MOIT.TestConfig(query=false)
+        MOIT.linear10test(solver, linconfig_noquery)
     end
 
     @testset "Linear Conic tests" begin
@@ -28,6 +32,15 @@ const MOIGLP = MathOptInterfaceGLPK
         intconfig = MOIT.TestConfig()
         solver_mip = GLPKOptimizerMIP()
         MOIT.intlineartest(solver_mip, intconfig, ["int2","int1"])
+    end
+
+    @testset "ModelLike tests" begin
+        intconfig = MOIT.TestConfig()
+        solver = XpressOptimizer()
+        MOIT.validtest(solver)
+        MOIT.emptytest(solver)
+        solver2 = XpressOptimizer()
+        MOIT.copytest(solver,solver2)
     end
 end
 ;
